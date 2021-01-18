1cd64c154886da3b80877aa2277c748aa1f747de
Svante-J-TE <54800689+Svante-J-TE@users.noreply.github.com>
2021-01-18 12:44:13 +0100

    crated human and method for animal ownership

--- SOLID-Labb/SOLID-Labb/SOLID-Labb/Dog.cs
+++ SOLID-Labb/SOLID-Labb/SOLID-Labb/Dog.cs
@@ -6,8 +6,6 @@ namespace SOLID_Labb
 {
     class Dog : Animal
     {
-        string _owner;
-
         public Dog(string color): base(color)
         {
             
@@ -15,7 +13,7 @@ namespace SOLID_Labb
 
         public override void Eat()
         {
-            Console.WriteLine("Dog is barking!");
+            Console.WriteLine("Dog is eating!");
         }
 
         public override void Sleep()
@@ -25,12 +23,7 @@ namespace SOLID_Labb
 
         public override void Speak()
         {
-            Console.WriteLine("Dog is eating. " + _owner +" is happy!");
-        }
-
-        public void TransferOwnership(string owner)
-        {
-            _owner = owner;
+            Console.WriteLine("Dog is barking!");
         }
     }
 }
---
+++ SOLID-Labb/SOLID-Labb/SOLID-Labb/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Labb
+{
+    class Human
+    {
+        public Human(string _name)
+        {
+            string name = _name;
+        }
+
+        public List<Animal> _animalsList = new List<Animal>();
+
+        public void gainAnimalOwnership(Animal animal)
+        {
+            _animalsList.Add(animal);
+        }
+    }
+}
